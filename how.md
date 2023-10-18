9a71fa95d4d4a87ee0fe7d6e2a18127926edf2d6
enduraew <enduraew@gmail.com>
2023-10-18 21:11:54 +0500

    Implement win and tie logic

--- how.md
+++ how.md
@@ -1,25 +1,79 @@
 # What we should compare?
-- main array board & combinationWin arr?
-OR
-- main array board & elements in DOM
-Need to compare 3 in a row certain marker (X or O)
+Массив должен быть не пустой и не выигрышним
 
-datacellWin = [
-[]
-]
+Cell.AddEventListener by click +
+IF win condition +
+  then 
+    print('win message') +
+    block gameboard +
+  end
+ELSE NOT WIN COMBO 
+  IF arr full
+    then    
+      print('tie')
+      block gameboard
+    end
+  ELSE NOT FULL ARR
+    make X draw marker
+    turn to O
+    *again call click listener*
+    make O draw marker
+    turn to X
+    *again call click listener*
 
-winCondition {
-  [0, 1, 2]
-}
 
-if (boardArr.length.3.includes('X') && [0, 1, 2] === boardArr) {
-    print WINS
-}
+turn X
+check if in cell no symbols
+if not make a marker X
+turn O
+check if in cell no symbols
+if not make a marker O
+turn X
+repeat
+check if arr is full
+if not full check if win combination?
+if no win combo and not full then next turn
+if full and no combination then tie
 
 
-boardArr return [0: 'X', 1: 'X', 2: 'X' ...]
+how check if board (arr) is has no spaces? (Full of X or O?)
 
-Every per-game turn check for win or tie
-if (boardArr === winCondition {
-then WIN
-}
+Что такое полный массив (игровая доска)? Это массив (игровая доска) в котором не осталось '' и есть только Х или О
+
+start game
+board is empty from ''? false ['', '', '', '', '', '', '', '', '']
+player1 marker X on cell of board //  ['X', '', '', '', '', '', '', '', ''] 
+board is empty from ''? false 
+player2 marker O on cell of board // ['X', '', 'O', '', '', '', '', '', ''] 
+board is empty from ''? false
+repeat til pre-end
+player1 marker X on cell of board  ['X', 'O', 'O', 'O', 'X', 'X', 'O', 'X', 'O'] 
+boars is empty from ''? true
+then log 'it's a tie!'
+
+how check if board (arr) is empty from ' '?
+
+Нужно каждую ячейку проверить на нахождение этого символа ' '
+После каждого хода нужно проверять доску на заполненность " "
+
+const isFullBoard = board.some(cell => cell === ' ') 
+Эта логика задает вопрос: "В доске еще остался элемент ' '?" 
+Таким образом если some найдет хоть одну ' ', то он кидает true и игра продолжается
+
+if (isFullBoard) {     
+    game continue
+} else 'its tie!'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,61 +1,122 @@
 'use strict';
 
-const gameBoard = (function () {
-  const arrCell = ['', '', '', '', '', '', '', '', ''];
+const gameBoard = (function() {
+    const arrCell = ['', '', '', '', '', '', '', '', ''];
 
-  return { arrCell };
+    return { arrCell };
 })();
 
 const Players = (name, marker) => {
-  return { name, marker };
+    return { name, marker };
 };
 
-const renderBoardDOM = (function () {
-  const _boardDiv = document.querySelector('.container-board');
+const renderBoardDOM = (function() {
+    const _boardDiv = document.querySelector('.container-board');
 
-  const renderCells = gameBoard.arrCell.forEach((element, id) => {
-    let _createCell = document.createElement('div');
-    _createCell.className = 'cell';
-    _createCell.textContent = element;
-    _boardDiv.appendChild(_createCell);
-    _createCell.setAttribute('data-cell', id);
-  });
-
-  return { renderCells };
-})();
-
-const gameController = (function () {
-  const humanPlayer = Players('Nikita', 'X');
-  const AIPlayer = Players('AI-3000', 'O');
-  const cells = document.querySelectorAll('.cell');
-  let _activePlayer = humanPlayer;
-
-  const _switchActivePlayer = () =>
-    _activePlayer === humanPlayer ? (_activePlayer = AIPlayer) : (_activePlayer = humanPlayer);
-
-  cells.forEach(cell => {
-    cell.addEventListener('click', e => {
-      if (cell.childNodes.length === 0) {
-        const targetCell = e.target.dataset.cell;
-        gameBoard.arrCell.splice(targetCell, 1, (e.target.textContent = _activePlayer.marker));
-        _switchActivePlayer();
-      }
+    const renderCells = gameBoard.arrCell.forEach((element, id) => {
+        let _createCell = document.createElement('div');
+        _createCell.className = 'cell';
+        _createCell.textContent = element;
+        _boardDiv.appendChild(_createCell);
+        _createCell.setAttribute('data-cell', id);
     });
-  });
 
-  return { humanPlayer, AIPlayer, _activePlayer, cells };
+    return { renderCells, _boardDiv };
 })();
 
-const checkGameOver = (function () {
-  // compare not the array but primitives
-  // 'some' method may helps when u need create 8 combinations
-  const winCondition = [[0, 1, 2]];
+const gameController = (function() {
+    const humanPlayer = Players('Nikita', 'X');
+    const AIPlayer = Players('AI-3000', 'O');
+    const cells = document.querySelectorAll('.cell');
+    let activePlayer = humanPlayer;
 
-  // const win1 = combinationWin[0] === gameBoard.arrCell
-  const log = () => {
-    const board = gameBoard.arrCell;
-    // board.slice(0, 3)
-    if (board[0]) return 'win';
-  };
-  return { winCondition, log };
+    const _switchActivePlayer = () =>
+        activePlayer === humanPlayer ? (activePlayer = AIPlayer) : (activePlayer = humanPlayer);
+
+
+    cells.forEach(cell => {
+        cell.addEventListener('click', (e) => {     
+            if (cell.childNodes.length === 0) {    
+              const targetCell = e.target.dataset.cell;
+              gameBoard.arrCell.splice(targetCell, 1, (e.target.textContent = activePlayer.marker));
+              _switchActivePlayer();
+              checkGameOver.xWin();
+              checkGameOver.oWin();
+            };
+        });
+    });
+
+    return { humanPlayer, AIPlayer, activePlayer, cells };
 })();
+
+const checkGameOver = (function() {
+    const _board = gameBoard.arrCell   
+    
+
+    const xWin = () => {
+        if (_board[0] === 'X' && _board[1] === 'X' && _board[2] === 'X') {
+            console.log(`${gameController.humanPlayer.name} is win!`);
+            gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+            return;
+        } else if (_board[3] === 'X' && _board[4] === 'X' && _board[5] === 'X') {
+            console.log(`${gameController.humanPlayer.name} is win!`);
+            gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+        } else if (_board[6] === 'X' && _board[7] === 'X' && _board[8] === 'X') {
+            console.log(`${gameController.humanPlayer.name} is win!`);
+            gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+        } else if (_board[0] === 'X' && _board[3] === 'X' && _board[6] === 'X') {
+            console.log(`${gameController.humanPlayer.name} is win!`);
+            gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+        } else if (_board[1] === 'X' && _board[4] === 'X' && _board[7] === 'X') {
+            console.log(`${gameController.humanPlayer.name} is win!`);
+            gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+        } else if (_board[2] === 'X' && _board[5] === 'X' && _board[8] === 'X') {
+            console.log(`${gameController.humanPlayer.name} is win!`);
+            gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+        } else if (_board[0] === 'X' && _board[4] === 'X' && _board[8] === 'X') {
+            console.log(`${gameController.humanPlayer.name} is win!`);
+            gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+        } else if (_board[2] === 'X' && _board[4] === 'X' && _board[6] === 'X') {
+            console.log(`${gameController.humanPlayer.name} is win!`);
+            gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+        } else tie();
+    };
+
+    const oWin = () => {
+        if (_board[0] === 'O' && _board[1] === 'O' && _board[2] === 'O') {
+            console.log(`${gameController.AIPlayer.name} is win!`);
+            gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+        } else if (_board[3] === 'O' && _board[4] === 'O' && _board[5] === 'O') {
+            console.log(`${gameController.AIPlayer.name} is win!`);
+            gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+        } else if (_board[6] === 'O' && _board[7] === 'O' && _board[8] === 'O') {
+            console.log(`${gameController.AIPlayer.name} is win!`);
+            gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+        } else if (_board[0] === 'O' && _board[3] === 'O' && _board[6] === 'O') {
+            console.log(`${gameController.AIPlayer.name} is win!`);
+            gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+        } else if (_board[1] === 'O' && _board[4] === 'O' && _board[7] === 'O') {
+            console.log(`${gameController.AIPlayer.name} is win!`);
+            gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+        } else if (_board[2] === 'O' && _board[5] === 'O' && _board[8] === 'O') {
+            console.log(`${gameController.AIPlayer.name} is win!`);
+            gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+        } else if (_board[0] === 'O' && _board[4] === 'O' && _board[8] === 'O') {
+            console.log(`${gameController.AIPlayer.name} is win!`);
+            gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+        } else if (_board[2] === 'O' && _board[4] === 'O' && _board[6] === 'O') {
+            console.log(`${gameController.AIPlayer.name} is win!`);
+            gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+        } else tie();
+    };
+
+    const tie = () => {
+       const _isFullBoard = _board.some(cell => cell === '');
+       if (!_isFullBoard && !xWin && !oWin) {
+         console.log('tie');
+        };
+    };
+
+    return { xWin, oWin, tie };
+})();
+
