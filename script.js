110ad4f9984527385b3b1a1e2b78149ad7012ffa
enduraew <enduraew@gmail.com>
2023-10-22 12:33:19 +0500

    Figure out how to make winning name. Instead of name i display X or O as name

--- script.js
+++ script.js
@@ -3,16 +3,14 @@
 const gameBoard = (function () {
   const arrCell = ['', '', '', '', '', '', '', '', ''];
 
-  return { arrCell }
+  return { arrCell };
 })();
 
 const Players = (name, marker) => {
-  return { name, marker };
-};
+ return { name, marker };
+}
 
 const renderBoardDOM = (function () {
-
-
   const _boardDiv = document.querySelector('.container-board');
 
   const renderCells = gameBoard.arrCell.forEach((element, id) => {
@@ -26,58 +24,22 @@ const renderBoardDOM = (function () {
   return { renderCells, _boardDiv };
 })();
 
-const getNamesFromForm = (function () {
-  const playersDiv = document.querySelector('.players-name');
-  const _form = document.querySelector('.name-form');
-  const _xPlayer = document.querySelector('#input-one');
-  const _oPlayer = document.querySelector('#input-two');
-  const _namesXO = [];
-
-  const playerOne = document.querySelector('.player-one');
-  const playerTwo = document.querySelector('.player-two');
-
-  const submitData = (function () {
-    _form.addEventListener('submit', el => {
-      el.preventDefault();
-      playersDiv.classList.add('hidden');
-      // renderPlayersName();
-      removeBlurBlock();
-
-      const valueOfX = _xPlayer.value;
-      const valuesOfO = _oPlayer.value;
-      _namesXO.push(valueOfX, valuesOfO);
-      // renderTurnText();
-      // renderBoardDOM._boardDiv.classList.remove('hidden');
-    });
-
-    // const renderTurnText = function() {
-    //  const whichTurnH2 = document.querySelector('.which-turn');
-    // whichTurnH2.classList.remove('hidden');
-    //whichTurnH2.textContent = `${gameController.activePlayer.name}, your turn`;
-    // };
-  })();
-
-  const removeBlurBlock = function () {
-    const blurDiv = document.querySelector('.blur-modal-board');
-    return blurDiv.remove();
-  };
-
-  /* const renderPlayersName = function() {
-   playerOne.textContent = _namesXO.x;
-   playerTwo.textContent = _namesXO.o;
-   playerOne.classList.remove('hidden');
-   playerTwo.classList.remove('hidden');
-};
-*/
-  return { submitData, _namesXO, playerOne, playerTwo };
-})();
-
 const checkGameOver = (function () {
   const _board = gameBoard.arrCell;
   const _textGameOver = document.querySelector('.gameover-h2');
 
-  const renderGameOver = function() {
-    _textGameOver.textContent = `${gameController._switchActivePlayer().name} is win!`;
+  const renderWinX = function () {
+    _textGameOver.textContent = `${gameController.humanPlayer.marker} is win!`;
+    _textGameOver.classList.remove('hidden');
+  };
+
+  const renderWinO = function () {
+    _textGameOver.textContent = `${gameController.AIPlayer.marker} is win!`;
+    _textGameOver.classList.remove('hidden');
+  };
+
+  const renderTie = function () {
+    _textGameOver.textContent = `Game over! It's tie!`;
     _textGameOver.classList.remove('hidden');
   };
 
@@ -87,74 +49,88 @@ const checkGameOver = (function () {
       gameBoard.arrCell[1] === 'X' &&
       gameBoard.arrCell[2] === 'X'
     ) {
-      console.log(`${gameController.humanPlayer.name} is win!`);
+      console.log(`${gameController.humanPlayer.marker} is win!`);
       gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
-      renderGameOver();
+      renderWinX();
       return;
     } else if (
       gameBoard.arrCell[3] === 'X' &&
       gameBoard.arrCell[4] === 'X' &&
       gameBoard.arrCell[5] === 'X'
     ) {
-      console.log(`${gameController.humanPlayer.name} is win!`);
+      console.log(`${gameController.humanPlayer.marker} is win!`);
       gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+      renderWinX();
       return;
     } else if (_board[6] === 'X' && _board[7] === 'X' && _board[8] === 'X') {
-      console.log(`${gameController.humanPlayer.name} is win!`);
+      console.log(`${gameController.humanPlayer.marker} is win!`);
       gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+      renderWinX();
       return;
     } else if (_board[0] === 'X' && _board[3] === 'X' && _board[6] === 'X') {
-      console.log(`${gameController.humanPlayer.name} is win!`);
+      console.log(`${gameController.humanPlayer.marker} is win!`);
       gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+      renderWinX();
       return;
     } else if (_board[1] === 'X' && _board[4] === 'X' && _board[7] === 'X') {
-      console.log(`${gameController.humanPlayer.name} is win!`);
+      console.log(`${gameController.humanPlayer.marker} is win!`);
       gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+      renderWinX();
       return;
     } else if (_board[2] === 'X' && _board[5] === 'X' && _board[8] === 'X') {
-      console.log(`${gameController.humanPlayer.name} is win!`);
+      console.log(`${gameController.humanPlayer.marker} is win!`);
       gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+      renderWinX();
       return;
     } else if (_board[0] === 'X' && _board[4] === 'X' && _board[8] === 'X') {
-      console.log(`${gameController.humanPlayer.name} is win!`);
+      console.log(`${gameController.humanPlayer.marker} is win!`);
       gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+      renderWinX();
       return;
     } else if (_board[2] === 'X' && _board[4] === 'X' && _board[6] === 'X') {
-      console.log(`${gameController.humanPlayer.name} is win!`);
+      console.log(`${gameController.humanPlayer.marker} is win!`);
       gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+      renderWinX();
       return;
     } else if (_board[0] === 'O' && _board[1] === 'O' && _board[2] === 'O') {
-      console.log(`${gameController.AIPlayer.name} is win!`);
+      console.log(`${gameController.AIPlayer.marker} is win!`);
       gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
-      renderGameOver();
+      renderWinO();
       return;
     } else if (_board[3] === 'O' && _board[4] === 'O' && _board[5] === 'O') {
-      console.log(`${gameController.AIPlayer.name} is win!`);
+      console.log(`${gameController.AIPlayer.marker} is win!`);
       gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+      renderWinO();
       return;
     } else if (_board[6] === 'O' && _board[7] === 'O' && _board[8] === 'O') {
-      console.log(`${gameController.AIPlayer.name} is win!`);
+      console.log(`${gameController.AIPlayer.marker} is win!`);
       gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+      renderWinO();
       return;
     } else if (_board[0] === 'O' && _board[3] === 'O' && _board[6] === 'O') {
-      console.log(`${gameController.AIPlayer.name} is win!`);
+      console.log(`${gameController.AIPlayer.marker} is win!`);
       gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+      renderWinO();
       return;
     } else if (_board[1] === 'O' && _board[4] === 'O' && _board[7] === 'O') {
-      console.log(`${gameController.AIPlayer.name} is win!`);
+      console.log(`${gameController.AIPlayer.marker} is win!`);
       gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+      renderWinO();
       return;
     } else if (_board[2] === 'O' && _board[5] === 'O' && _board[8] === 'O') {
-      console.log(`${gameController.AIPlayer.name} is win!`);
+      console.log(`${gameController.AIPlayer.marker} is win!`);
       gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+      renderWinO();
       return;
     } else if (_board[0] === 'O' && _board[4] === 'O' && _board[8] === 'O') {
-      console.log(`${gameController.AIPlayer.name} is win!`);
+      console.log(`${gameController.AIPlayer.marker} is win!`);
       gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+      renderWinO();
       return;
     } else if (_board[2] === 'O' && _board[4] === 'O' && _board[6] === 'O') {
-      console.log(`${gameController.AIPlayer.name} is win!`);
+      console.log(`${gameController.AIPlayer.marker} is win!`);
       gameController.cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+      renderWinO();
       return;
     }
     return tie();
@@ -162,37 +138,81 @@ const checkGameOver = (function () {
 
   const tie = function () {
     if (gameBoard.arrCell.some(cl => cl === '')) {
-      return;
-    } else return console.log('tie');
+      return gameController._switchActivePlayer();
+    } else {
+      console.log('tie!');
+      renderTie();
+    }
   };
 
   return { win, tie };
 })();
 
+const getNamesFromForm = (function () {
+  const _form = document.querySelector('.name-form');
+  const playersDiv = document.querySelector('.players-name');
+  let _xPlayer = document.querySelector('#input-one');
+  let _oPlayer = document.querySelector('#input-two');
+
+  const textBottomPlayer1 = document.querySelector('.text-bottom-player1');
+  const textBottomPlayer2= document.querySelector('.text-bottom-player2');
+
+  const removeBlurBlock = function () {
+    const blurDiv = document.querySelector('.blur-modal-board');
+    return blurDiv.remove();
+  };
+
+  let _namesXO = [];
+
+  _form.addEventListener('submit', el => {
+     el.preventDefault();
+     playersDiv.classList.add('hidden');
+     removeBlurBlock();
+     _namesXO.push(`${_xPlayer.value}`, `${_oPlayer.value}`); 
+     renderPlayersName();
+   });
+
+
+  const renderPlayersName = function () {
+    textBottomPlayer1.textContent = `X: ${_namesXO[0]}`;
+    textBottomPlayer2.textContent = `O: ${_namesXO[1]}`;
+    textBottomPlayer1.classList.remove('hidden');
+    textBottomPlayer2.classList.remove('hidden');
+
+  };
+
+  return { renderPlayersName, _namesXO };
+})();
+
+
 const gameController = (function () {
-  const humanPlayer = Players(`Nikita`, `X`);
-  const AIPlayer = Players(`AI`, `O`);
+  const humanPlayer = Players(`Human Player 1`, `X`);
+  const AIPlayer = Players(`Human Player 2`, `O`);
   const cells = document.querySelectorAll('.cell');
   let activePlayer = humanPlayer;
 
   const _switchActivePlayer = () => {
     if (activePlayer === humanPlayer) {
-       return activePlayer = AIPlayer;
-    } else return activePlayer = humanPlayer;
+      return (activePlayer = AIPlayer);
+    } else return (activePlayer = humanPlayer);
   };
-
 
   cells.forEach(cell => {
     cell.addEventListener('click', e => {
       if (cell.childNodes.length === 0) {
         const targetCell = e.target.dataset.cell;
         gameBoard.arrCell.splice(targetCell, 1, (e.target.textContent = activePlayer.marker));
-        checkGameOver.win()
-        _switchActivePlayer();
-        // getNamesFromForm.renderPlayersName();
+        checkGameOver.win();
       }
     });
   });
 
-  return { humanPlayer, AIPlayer, activePlayer, cells, _switchActivePlayer };
+  return { activePlayer, cells, _switchActivePlayer, humanPlayer, AIPlayer };
+})();
+
+const restart = (function () {
+  const restartBtn = document.querySelector('.restart-btn');
+  restartBtn.addEventListener('click', () => {
+    window.location.reload();
+  });
 })();
